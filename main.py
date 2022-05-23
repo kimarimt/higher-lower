7e3598448fb3c709fd7288729d3a1c58783f90ae
Alec German <kimarimt@protonmail.com>
2022-05-22 20:09:07 -0400

    adding finishing touches and some ascii art

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import random
 import games
 import os
 import time
+import art
 
 
-def clear():
-    time.sleep(1)
+def clear(secs=0):
+    time.sleep(secs)
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
@@ -20,19 +21,21 @@ def main():
         gameA = top_games[progress]
         gameB = top_games[progress + 1]
 
+        print(art.logo)
         print(f'Compare A: {gameA["name"]}')
+        print(art.vs)
         print(f'Compare B: {gameB["name"]}')
 
         guess = input(
             'Which game is higher on the Top 20 list (\'a\' or \'b\'): ').lower()
         if gameA['position'] < gameB['position'] and guess == 'a':
             score += 1
-            clear()
-            print(f'Correct! Current Score: {score}')
+            print(f'\nCorrect! Current Score: {score}')
+            clear(secs=1)
         elif gameB['position'] < gameA['position'] and guess == 'b':
             score += 1
-            clear()
-            print(f'Correct! Current Score: {score}')
+            print(f'\nCorrect! Current Score: {score}')
+            clear(secs=1)
         else:
             clear()
             print(f'Sorry that\'s incorrect. Final score: {score}')
